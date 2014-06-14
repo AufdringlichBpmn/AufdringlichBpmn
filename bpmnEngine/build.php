a50230d3b381a38adaaf8385ea21b86bdb6b29d1
Christoph Dolch <bpmn@aufdringlich.de>
2014-06-14 20:50:16 +0200

    Buildfile nachgezogen

--- bpmnEngine/build.php
+++ bpmnEngine/build.php
@@ -11,5 +11,9 @@ $phar ["XmlAdapter.php"] = file_get_contents($srcRoot."/XmlAdapter.php");
 $phar ["CouchDbStore.php"] = file_get_contents($srcRoot."/CouchDbStore.php");
 $phar ["InMemoryStore.php"] = file_get_contents($srcRoot."/InMemoryStore.php");
 $phar ["FileStore.php"] = file_get_contents($srcRoot."/FileStore.php");
+$phar ["elements/DefaultElementHandler.php"] = file_get_contents($srcRoot."/elements/DefaultElementHandler.php");
+$phar ["elements/EventHandler.php"] = file_get_contents($srcRoot."/elements/EventHandler.php");
+$phar ["elements/GatewayHandler.php"] = file_get_contents($srcRoot."/elements/GatewayHandler.php");
+$phar ["elements/TaskHandler.php"] = file_get_contents($srcRoot."/elements/TaskHandler.php");
 
 $phar->setStub($phar->createDefaultStub("BpmnEngine.php"));
