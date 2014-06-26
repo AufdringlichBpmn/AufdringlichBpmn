43d9e1f3ed4e74d2063db656b483c988b5f3a700
Christoph Dolch <bpmn@aufdringlich.de>
2014-06-26 08:24:00 +0200

    ...progress...

--- bpmnEngine/BpmnEngine.php
+++ bpmnEngine/BpmnEngine.php
@@ -2,37 +2,37 @@
 require_once ('elements/DefaultElementHandler.php');
 
 class BpmnEngine{
-	private $dbAdapter;
+	private $storage;
 	private $xmlAdapter;
 	private $name;
 
-	function __construct(ProcessStore $dbAdapter, $name) {
-		$this->dbAdapter = $dbAdapter;
+	function __construct(ProcessStore $storage, $name) {
+		$this->storage = $storage;
 		$this->name = $name;
 		$this->xmlAdapter = new XmlAdapter();
 	}
 
 	public function importDefinition($process_definition_xml) {
-		$this->dbAdapter->importDefinition($process_definition_xml);
+		$this->storage->importDefinition($process_definition_xml);
 	}
 
 	public function startProcess($variables) {
-		$processDefinition = $this->dbAdapter->loadProcessDefinition($this->name);
+		$processDefinition = $this->storage->loadProcessDefinition($this->name);
 		$process = ProcessInstance::buildByProcessDefinition($this, $processDefinition, $this->name);
 		foreach($variables as $key => $value){
 			$process->put($key,$value);
 		}
 		$process->start();
-		$this->dbAdapter->storeProcess($process);
+		$this->storage->storeProcess($process);
 		return $process;
 	}
 	public function executeUserTaskByRefId($process, $refId, $value = null){
 		$process->executeUserTaskByRefId($refId, $value);
-		$this->dbAdapter->storeProcess($process);
+		$this->storage->storeProcess($process);
 	}
 	function loadProcess($processId){
-		$processDefinition = $this->dbAdapter->loadProcessDefinition($this->name);
-		$processDto = $this->dbAdapter->loadProcess($processId);
+		$processDefinition = $this->storage->loadProcessDefinition($this->name);
+		$processDto = $this->storage->loadProcess($processId);
 		$process = ProcessInstance::buildByDto($this, $processDefinition->xml, $processDto);
 		return $process;
 	}
@@ -40,13 +40,13 @@ class BpmnEngine{
 		$process = $this->loadProcess($processId);
 		while($process->processNextUserTask()); // all evaluated usertasks
 		while($process->processNextEvent()); // all evaluated events 
-		$this->dbAdapter->storeProcess($process);
+		$this->storage->storeProcess($process);
 		return $process;
 	}
 
 	function findNotExecutedProcessInstanceIds(){
 		// TODO filter nach ProcessDefinition
-		return $this->dbAdapter->findNotExecutedProcessInstanceIds();
+		return $this->storage->findNotExecutedProcessInstanceIds();
 	}
 
 	private static $bpmnElemenHandlerMap = array();
