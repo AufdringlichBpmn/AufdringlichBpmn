1b6d6435269da0c239a0de85b8f3de248099d127
Christoph Dolch <bpmn@aufdringlich.de>
2014-08-22 08:26:58 +0200

    Implementing new Workflow

--- bpmnManager/client.js
+++ bpmnManager/client.js
@@ -55,11 +55,30 @@ $(document).ready(function() {
 			data: {
 				process_definition_id: $(this).closest("[data-processDefinitionId]").attr("data-processDefinitionId")
 			},
-			success: function(data){
+			success: function(process){
 				var template = $("#template-startProcessDialog").text();
 				$("#startProcessDialog [role='main']").html(
-					Mustache.render(template, data)
+					Mustache.render(template, process)
 				);
+				// search next open user task
+				// and set ok-button to opentasks
+				$.ajax({
+					url: config.server.base_url + config.server.open_usertasks_url,
+					data: {},
+					success: function(data){
+						$.each( data.usertasks, function( index, task ){
+							if(task.process_id == process.process._id){
+								// change href to new  o u t
+								$("#executeUserTask").attr("data-processDefinitionId", task.process_definition_id);
+								$("#executeUserTask").attr("data-processId", task.process_id);
+								$("#executeUserTask").attr("data-taskId", task.id);
+								$("#startProcessDialog [role='main'] .ok").attr("href","#executeUserTask");
+								return;
+							}
+						});
+					},
+					dataType: "json"
+				});
 			},
 			dataType: "json"
 		});
@@ -78,23 +97,49 @@ $(document).ready(function() {
 					Mustache.render(template, data)
 				);
 				$( "a[data-action='executeUserTask-save']" ).click(function( event ) {
-					var result = $(this).attr("data-value");
+					var atag = $(this);
+					var result = atag.attr("data-value");
+					var processId = atag.closest("[data-processId]").attr("data-processId");
 					var vars = {};
-					$(this).closest("div").find(":input[data-name]").each(function(i,e){
-						vars[$(this).attr("data-name")] = $(this).val();
+					atag.closest("div").find(":input[data-name]").each(function(i,e){
+						vars[atag.attr("data-name")] = atag.val();
 					});
+					var done = false;
 					$.ajax({
 						url: config.server.base_url + config.server.evaluate_usertask_url,
 						data: {
-							process_id: $(this).closest("[data-processId]").attr("data-processId"),
-							task_id: $(this).closest("[data-taskId]").attr("data-taskId"),
+							process_id: processId,
+							task_id: atag.closest("[data-taskId]").attr("data-taskId"),
 							vars: JSON.stringify(vars),
 							result: result
 						},
 						success: function(data){
+							// search next open user task
+							$.ajax({
+								url: config.server.base_url + config.server.open_usertasks_url,
+								data: {},
+								success: function(data){
+									var found = false;
+									$.each( data.usertasks, function( index, task ){
+										if(task.process_id == processId){
+											// change href to new  o u t
+											$("#executeUserTask").attr("data-taskId", task.id);
+											$("#executeUserTask").trigger( "pageshow");
+											found = true;
+											return;
+										}
+									});
+									if(!found) location.hash="#pageUserTasks";
+								},
+								error: function(data){
+									location.hash="#pageUserTasks";
+								},
+								dataType: "json"
+							});
 						},
 						dataType: "json"
 					});
+					return false;
 				});
 				$( "#executeUserTask-cancel" ).click(function( event ) {
 				});
